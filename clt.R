6b4efa466872b2f484f9888f876dd7586209f712
bonochof <ryos427@yahoo.co.jp>
2018-11-07 19:33:24 +0900

    follow uniform distribution

--- clt.R
+++ clt.R
@@ -1,8 +1,20 @@
 # Central Limit Theorem (CLT)
+
+# Normal distribution
 n.population <- 100000
 mu.population <- 10
 sd.population <- 1
 population <- rnorm(n.population, mean=mu.population, sd=sd.population)
+mu.population <- mean(population)
+var.population <- var(population)
+hist(population)
+abline(v=mu.population, col="red", lwd=2)
+
+# Uniform distribution
+n.population <- 100000
+min.population <- 0
+max.population <- 20
+population <- runif(n.population, min=min.population, max=max.population)
 mu.population <- mean(population)
 var.population <- var(population)
 hist(population)
