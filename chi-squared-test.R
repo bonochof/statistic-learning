4b62cdf32adb978f776cfc5844f25170b2a1da01
bonochof <ryos427@yahoo.co.jp>
2018-11-13 22:45:11 +0900

    test of independence

--- chi-squared-test.R
+++ chi-squared-test.R
@@ -1,3 +1,7 @@
 # test of goodness of fit
 vx <- c(58, 99, 32, 48)
 chisq.test(x=vx, p=c(0.3, 0.5, 0.1, 0.1))
+
+# test of independence
+vx <- matrix(c(43, 20, 28, 15, 31, 11), nrow=2, byrow=T)
+chisq.test(vx)
